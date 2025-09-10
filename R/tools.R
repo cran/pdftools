6bc37d01583a19329f9f439c581db7f8ffe7c715
Jeroen Ooms <jeroenooms@gmail.com>
2025-09-10 07:50:15 +0000

    version 3.6.0

--- R/tools.R
+++ R/tools.R
@@ -41,9 +41,10 @@ pdf_info <- function(pdf, opw = "", upw = "") {
 }
 
 #' @rdname pdftools
+#' @param raw return text in raw stream order. Default is to use physical layout order.
 #' @export
-pdf_text <- function(pdf, opw = "", upw = "") {
-  poppler_pdf_text(loadfile(pdf), opw, upw)
+pdf_text <- function(pdf, opw = "", upw = "", raw = FALSE) {
+  poppler_pdf_text(loadfile(pdf), opw, upw, raw)
 }
 
 #' @rdname pdftools
